08c4ee37b2ba4a49ab01d54742ca4c3b70eba72e
Laura Powles <nebulaeandstars@gmail.com>
2021-03-21 06:16:55 +1100

    added base config

--- config.def.h
+++ config.def.h
@@ -4,14 +4,15 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"monospace:size=12",
+    "Noto Fonts Emoji:pixelsize=12:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#ffffff", "#2d2d2d" },
+	[SchemeSel] = { "#2d2d2d", "#ffd8ff" },
+	[SchemeOut] = { "#2d2d2d", "#d8ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
