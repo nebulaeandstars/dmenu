274e74c81058e45577ed88238ca2071d7eaeae39
Laura Powles <nebulaeandstars@gmail.com>
2021-03-22 00:29:24 +1100

    switched to JoyPixels by default for emoji

--- config.def.h
+++ config.def.h
@@ -6,7 +6,8 @@ static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't us
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=12",
-    "Noto Fonts Emoji:pixelsize=12:antialias=true:autohint=true"
+    "JoyPixels:pixelsize=12:antialias=true:autohint=true",
+    "NotoColorEmoji:pixelsize=12:antialias=true:autohint=true",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
