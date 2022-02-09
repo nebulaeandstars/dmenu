c7ae1ad777c4c91309d110f1a492266b929dfd18
Laura Powles <laura@laurapowles.com>
2022-02-09 03:49:42 +0000

    added personal settings to config.def.h

--- config.def.h
+++ config.def.h
@@ -5,16 +5,18 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"monospace:size=12",
+    "JoyPixels:pixelsize=12:antialias=true:autohint=true",
+    "NotoColorEmoji:pixelsize=12:antialias=true:autohint=true",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#a5a5a5", "#2d2d2d" },
+	[SchemeSel] = { "#2d2d2d", "#ffd8ff" },
+ 	[SchemeSelHighlight] = { "#a5a5a5", "#ffd8ff" },
+ 	[SchemeNormHighlight] = { "#ffffff", "#2d2d2d" },
+	[SchemeOut] = { "#2d2d2d", "#d8ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
